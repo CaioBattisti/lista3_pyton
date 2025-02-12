4303c5bcb80769a2e0a5ffa76c2f6f35e6f637fd
CaioBattisti <cluizbattisti@gmail.com>
2025-02-12 19:10:50 -0300

    finalizado 5

--- exercicio5_Caio Luiz Battisti.py
+++ exercicio5_Caio Luiz Battisti.py
@@ -1,13 +1,13 @@
 #Pergunte ao usuário se está chovendo e converta sua resposta em minúsculas para que não importe em que caso ele digite.
-#Se ele responder "sim", pergunte se está ventando. Se ele responder "sim" a esta segunda pergunta, exiba a resposta "Está ventando muito para um guarda-chuva"
-#caso contrário, exiba a mensagem "Pegue um guarda-chuva".
+#Se ele responder "sim", pergunte se está ventando. Se ele responder "sim" a esta segunda pergunta, exiba a resposta 
+#"Está ventando muito para um guarda-chuva",caso contrário, exiba a mensagem "Pegue um guarda-chuva".
 #Se ele não respondera sim à primeira pergunta, mostre a resposta "Aproveite o seu dia".
-clima = input("Esta chovendo ai?")
-if clima == ("nao"):
-    print("Aproveite seu dia")
-if clima == ("sim"):
-    clima2 = input("Esta ventando muito?")
-elif clima == ("nao"):
-    print("Leve um guarda-chuva")
+clima = input("Esta chovendo ai?").strip().lower()
+if clima =="sim":
+    clima = input("esta ventando?").strip().lower()
+    if clima == "sim":
+        print("esta ventando muito para um guarda-chuva.")
+    else:
+        print("pegue um charda-chuva.")
 else:
-    print("Esta ventando muito para um guarda-chuva")
+    print("aproveite seu dia.")
