1f5de75031325c25b9e4a00a0cfb6002f6748a85
CaioBattisti <cluizbattisti@gmail.com>
2025-02-12 21:38:56 -0300

    finalizar 9

--- exercicio9_Caio Luiz Battisti.py
+++ exercicio9_Caio Luiz Battisti.py
@@ -1,6 +1,7 @@
 #Escreva um programa que faça o cálculo do imposto de renda 2025. Consulte a tabela no site da Receita federal.
 renda = input("digite sua renda: ")
-if renda <= 2259.20:
+if renda < 2259.20:
     print("sem desconto de renda.")
-if renda > 2259.20 and renda <= 2826.65:
-    float("foi descontado",renda *0.075)
+if renda >= 2259.20 and renda <= 2826.65:
+    imposto = renda * 0.075
+    print("foi descontado",imposto)
